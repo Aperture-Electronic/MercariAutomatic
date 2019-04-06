e2be5eb4a6d3ebdec230f69fdba2d728a84def16
小野寺つさき <ApertureElectronic@outlook.com>
2019-04-06 15:57:24 +0800

    Create the project framework

---
+++ MercariAutomatic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MercariAutomatic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
